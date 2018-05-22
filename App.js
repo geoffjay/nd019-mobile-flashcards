3f0dd0e742be31767f97b5fddb26b99ceb27cedf
Geoff Johnson <geoff.jay@gmail.com>
2018-05-21 22:18:54 -0700

    feat: Add packages with native components

--- App.js
+++ App.js
@@ -1,23 +1,92 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, View, StatusBar, Dimensions } from 'react-native'
+import Color from 'react-native-material-color'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import {
+  createMaterialTopTabNavigator,
+  createStackNavigator
+} from 'react-navigation'
+import { Constants } from 'expo'
+import reducer from './reducers'
+import DeckList from './components/DeckList'
+import AddDeck from './components/AddDeck'
+import Deck from './components/Deck'
+import Quiz from './components/Quiz'
+import AddCard from './components/AddCard'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+function CardsStatusBar ({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const Tabs = createMaterialTopTabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'List',
+    },
   },
-});
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add',
+    },
+  },
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Color.White,
+    style: {
+      height: 56,
+      backgroundColor: Color.DeepOrange,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: Tabs,
+    navigationOptions: {
+      header: null,
+    },
+  },
+  Deck: { screen: Deck },
+  Quiz: { screen: Quiz },
+  AddCard: { screen: AddCard },
+}, {
+  headerMode: 'screen',
+  mode: 'modal',
+  navigationOptions: {
+    headerTintColor: Color.White,
+    headerStyle: {
+      backgroundColor: Color.DeepOrange,
+      height: 56,
+    },
+  },
+})
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <CardsStatusBar backgroundColor={Color.DeepOrange} barStyle="light-content" />
+          <MainNavigator />
+        </View>
+      </Provider>
+    )
+  }
+}
