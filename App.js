796b3a4a01590de6cfd3eb538269b59dbc8f2fdf
Geoff Johnson <geoff.jay@gmail.com>
2018-05-27 12:34:29 -0700

    refactor: Move app view into base component

--- App.js
+++ App.js
@@ -1,92 +1,19 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, StatusBar, Dimensions } from 'react-native'
-import Color from 'react-native-material-color'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import {
-  createMaterialTopTabNavigator,
-  createStackNavigator
-} from 'react-navigation'
-import { Constants } from 'expo'
-import reducer from './reducers'
-import DeckList from './components/DeckList'
-import AddDeck from './components/AddDeck'
-import Deck from './components/Deck'
-import Quiz from './components/Quiz'
-import AddCard from './components/AddCard'
+import configureStore from './configureStore'
+import FlashCards from './containers/FlashCards'
 
-function CardsStatusBar ({ backgroundColor, ...props }) {
-  return (
-    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
-      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-    </View>
-  )
-}
+const store = configureStore()
 
-const Tabs = createMaterialTopTabNavigator({
-  DeckList: {
-    screen: DeckList,
-    navigationOptions: {
-      tabBarLabel: 'List',
-    },
-  },
-  AddDeck: {
-    screen: AddDeck,
-    navigationOptions: {
-      tabBarLabel: 'Add',
-    },
-  },
-}, {
-  navigationOptions: {
-    header: null
-  },
-  tabBarOptions: {
-    activeTintColor: Color.White,
-    style: {
-      height: 56,
-      backgroundColor: Color.DeepOrange,
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-})
-
-const MainNavigator = createStackNavigator({
-  Home: {
-    screen: Tabs,
-    navigationOptions: {
-      header: null,
-    },
-  },
-  Deck: { screen: Deck },
-  Quiz: { screen: Quiz },
-  AddCard: { screen: AddCard },
-}, {
-  headerMode: 'screen',
-  mode: 'modal',
-  navigationOptions: {
-    headerTintColor: Color.White,
-    headerStyle: {
-      backgroundColor: Color.DeepOrange,
-      height: 56,
-    },
-  },
-})
-
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <Provider store={createStore(reducer)}>
-        <View style={{flex: 1}}>
-          <CardsStatusBar backgroundColor={Color.DeepOrange} barStyle="light-content" />
-          <MainNavigator />
-        </View>
+      <Provider store={store}>
+        <FlashCards />
       </Provider>
     )
   }
 }
+
+export default App
--- components/FlashCards.js
+++ components/FlashCards.js
@@ -3,76 +3,15 @@ import {
   Dimensions,
   StyleSheet,
   View,
+  //AsyncStorage
 } from 'react-native'
 import Color from 'react-native-material-color'
-import {
-  createMaterialTopTabNavigator,
-  createStackNavigator
-} from 'react-navigation'
 import { CardsStatusBar } from './CardsStatusBar'
-import AddDeck from '../containers/AddDeck'
-import DeckList from './DeckList'
-import Deck from './Deck'
-import Quiz from './Quiz'
-import AddCard from './AddCard'
-
-const Tabs = createMaterialTopTabNavigator({
-  DeckList: {
-    screen: DeckList,
-    navigationOptions: {
-      tabBarLabel: 'List',
-    },
-  },
-  AddDeck: {
-    screen: AddDeck,
-    navigationOptions: {
-      tabBarLabel: 'Add',
-    },
-  },
-}, {
-  navigationOptions: {
-    header: null
-  },
-  tabBarOptions: {
-    activeTintColor: Color.White,
-    style: {
-      height: 56,
-      backgroundColor: Color.DeepOrange,
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-})
-
-const MainNavigator = createStackNavigator({
-  Home: {
-    screen: Tabs,
-    navigationOptions: {
-      header: null,
-    },
-  },
-  Deck: { screen: Deck },
-  Quiz: { screen: Quiz },
-  AddCard: { screen: AddCard },
-}, {
-  headerMode: 'screen',
-  mode: 'modal',
-  navigationOptions: {
-    headerTintColor: Color.White,
-    headerStyle: {
-      backgroundColor: Color.DeepOrange,
-      height: 56,
-    },
-  },
-})
+import { MainNavigator } from './Navigation'
 
 class FlashCards extends Component {
   componentDidMount() {
+    //AsyncStorage.clear()
     this._initialData()
     console.log(this.props.decks)
   }
@@ -82,13 +21,9 @@ class FlashCards extends Component {
   }
 
   render() {
-    // XXX move this
-    // const { decks } = this.props
-
     return (
       <View style={styles.container}>
         <CardsStatusBar backgroundColor={Color.DeepOrange} barStyle="light-content" />
-        {/*<List decks={decks}/>*/}
         <MainNavigator />
       </View>
     )
